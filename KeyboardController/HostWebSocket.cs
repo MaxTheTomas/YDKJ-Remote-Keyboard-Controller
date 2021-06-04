bfcd127daeec3ca21dcb9bd705b725ea94dd3ebb
MaxTheTomas <48125186+MaxTheTomas@users.noreply.github.com>
2021-06-04 20:29:35 +0300

    First Working Version
    Works with WebSockets

---
+++ KeyboardController/HostWebSocket.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Diagnostics;
+using Gma.System.MouseKeyHook;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
+using WebSocket = WebSocketSharp.WebSocket;
+
+namespace KeyboardController {
+    public class HostWebSocket {
+        private WebSocketServer server;
+        public HostWebSocket() {
+            server = new WebSocketServer(8001); 
+            server.AddWebSocketService<Receiver>("/");
+            server.Start();
+        }
+        
+        
+        public void Close() {
+            server.Stop();
+            server.RemoveWebSocketService("/");
+            server = null;
+        }
+        
+        class Receiver : WebSocketBehavior {
+            [DllImport ("User32.dll")]
+            static extern int SetForegroundWindow(IntPtr point);
+
+            protected override void OnMessage(MessageEventArgs e) {
+                SendKeys.SendWait(e.Data);
+            }
+        }
+    }
+}
---
+++ KeyboardController/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Input;
+using Gma.System.MouseKeyHook;
+using WebSocketSharp;
+using WebSocket = WebSocketSharp.WebSocket;
+
+namespace KeyboardController {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+            IKeyboardMouseEvents m = Hook.GlobalEvents();
+            m.KeyDown += (o, a) => { if (a.KeyCode == Keys.Home) Environment.Exit(0); };
+            KeyDown += (o, a) => {
+                if (client != null && client.ReadyState == WebSocketState.Open) {
+                    if (a.Key != Key.Home) {
+                        if (a.Key.ToString().Length == 1) {
+                            client.Send(a.Key.ToString());
+                        }
+                        if (a.Key.ToString().Length == 2) {
+                            if (int.TryParse(a.Key.ToString().Remove(0, 1), out _)) {
+                                client.Send(a.Key.ToString().Replace("D", ""));
+                            }
+                        }
+                    }
+                }
+            };
+        }
+
+        private HostWebSocket host;
+
+        public void StartServer(object sender, RoutedEventArgs routedEventArgs) {
+            host = new HostWebSocket();
+            StartS.IsEnabled = false;
+            StartC.IsEnabled = false;
+            StopC.IsEnabled = false;
+        }
+
+        public void StopServer(object sender, RoutedEventArgs routedEventArgs) {
+            if (host != null) host.Close();
+            host = null;
+            
+            StartS.IsEnabled = true;
+            StartC.IsEnabled = true;
+            StopC.IsEnabled = true;
+        }
+
+        private WebSocket client;
+
+        public void StartClient(object sender, RoutedEventArgs routedEventArgs) {
+            StartC.IsEnabled = false;
+            StartS.IsEnabled = false;
+            StopS.IsEnabled = false;
+            IpBox.IsEnabled = false;
+            
+            try {
+                client = new WebSocket($"ws://{IpBox.Text}:8001/");
+                client.Connect();
+            } catch (Exception e) {
+                Error.Content = e.Message;
+            }
+        }
+
+        public void StopClient(object sender, RoutedEventArgs routedEventArgs) {
+            if (client != null) {
+                client.Close();
+                client = null;
+            }
+            
+            IpBox.IsEnabled = true;
+            StartC.IsEnabled = true;
+            StartS.IsEnabled = true;
+            StopS.IsEnabled = true;
+        }
+    }
+}
